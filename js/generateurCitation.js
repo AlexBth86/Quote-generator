343e42f2e868f0c967b4409291063d1400c11ab8
AlexBth86 <alexandreberthon@gmail.com>
2019-01-11 17:37:31 +0100

    First commit

---
+++ js/generateurCitation.js
@@ -0,0 +1,273 @@
+// Protoype citation
+
+	var citation = {
+	
+		init : function (texte) {
+			this.texte = texte;
+		},
+	};
+
+// Tableaux citations
+
+	
+	// Tableau citations intelligentes
+
+		
+		// Intros intelligentes
+
+
+		var intro1 = Object.create (citation);
+		intro1.init("Tant que durera la fragilité actuelle,");
+
+		var intro2 = Object.create (citation);
+		intro2.init("Pour réagir face à complexité induite,");
+
+		var intro3 = Object.create (citation);
+		intro3.init("Eu égard à la morosité de ces derniers temps,");
+
+		var intro4 = Object.create (citation);
+		intro4.init("Vu l'orientation intrinsèque,");
+
+		var intro5 = Object.create (citation);
+		intro5.init("Malgré la dégradation induite,");
+
+		var intro6 = Object.create (citation);
+		intro6.init("Nonobstant la situation contextuelle,");
+
+		var introAll = [];
+		introAll.push (intro1);
+		introAll.push (intro2);
+		introAll.push (intro3);
+		introAll.push (intro4);
+		introAll.push (intro5);
+		introAll.push (intro6);
+
+		
+		// Corps intelligentes
+
+		
+		var corps1 = Object.create (citation);
+		corps1.init("on se doit d'essayer chacune des voies pertinentes,");
+
+		var corps2 = Object.create (citation);
+		corps2.init("on ne peut se passer d'uniformiser les relations des hypothèses pertinentes,");
+
+		var corps3 = Object.create (citation);
+		corps3.init("nous sommes contraints d'examiner la somme des options de bon sens,");
+
+		var corps4 = Object.create (citation);
+		corps4.init("il convient de prendre en considération les principales hypothèses possibles,");
+
+		var corps5 = Object.create (citation);
+		corps5.init("il ne faut pas s'interdire d'essayer chacune des stratégies imaginables,");
+
+		var corps6 = Object.create (citation);
+		corps6.init("je n'exclus pas d'inventorier la plus grande partie des actions réalisables,");
+
+
+		var corpsAll = [];
+		corpsAll.push (corps1);
+		corpsAll.push (corps2);
+		corpsAll.push (corps3);
+		corpsAll.push (corps4);
+		corpsAll.push (corps5);
+		corpsAll.push (corps6);
+
+		
+		// Conslusions intelligentes
+
+		var conclusion1 = Object.create (citation);
+		conclusion1.init("si l'on veut s'en sortir un jour.");
+
+		var conclusion2 = Object.create (citation);
+		conclusion2.init("parce qu'il s'agit de notre dernière chance.");
+
+		var conclusion3 = Object.create (citation);
+		conclusion3.init("même si ce n'est pas facile.");
+
+		var conclusion4 = Object.create (citation);
+		conclusion4.init("avec toute la prudence requise.");
+
+		var conclusion5 = Object.create (citation);
+		conclusion5.init("à long terme.");
+
+		var conclusion6 = Object.create (citation);
+		conclusion6.init("en prenant toutes les précautions qui s'imposent.");
+
+
+		var conclusionAll = [];
+		conclusionAll.push (conclusion1);
+		conclusionAll.push (conclusion2);
+		conclusionAll.push (conclusion3);
+		conclusionAll.push (conclusion4);
+		conclusionAll.push (conclusion5);
+		conclusionAll.push (conclusion6);
+
+
+	// Tableau citations Problème Réveil
+
+
+		// Intros excuses reveil
+
+
+		var introExcuse1 = Object.create (citation);
+		introExcuse1.init("En pleine nuit, pendant que je m'étais endormis sur un article fort intéressant du Herald Tribune,");
+
+		var introExcuse2 = Object.create (citation);
+		introExcuse2.init("Vers 4h du matin, pendant que je somnollais après avoir passé en revue une étude du Figaro Economique,");
+
+		var introExcuse3 = Object.create (citation);
+		introExcuse3.init("En pleine nuit, pendant que je faisais un somme après avoir fini de traduire Guerre & Paix en Mandarin,");
+
+		var introExcuse4 = Object.create (citation);
+		introExcuse4.init("Hier soir, au moment où je me reposais après avoir pratiqué 2h intenses de Squash,");
+
+		var introExcuse5 = Object.create (citation);
+		introExcuse5.init("Tard dans la nuit, au moment où je m'étais assoupi sur la brillante émission 'Chasse et Pêche',");
+
+		var introExcuse6 = Object.create (citation);
+		introExcuse6.init("Tôt dans la matinée, au moment où je sommeillais en attendant de me lever pour mon footing quotidien de 5h du matin,");
+
+		var introExcuseAll = [];
+		introExcuseAll.push (introExcuse1);
+		introExcuseAll.push (introExcuse2);
+		introExcuseAll.push (introExcuse3);
+		introExcuseAll.push (introExcuse4);
+		introExcuseAll.push (introExcuse5);
+		introExcuseAll.push (introExcuse6);
+
+		// Corps excuses reveil
+
+		var corpsExcuse1 = Object.create (citation);
+		corpsExcuse1.init("mon sanglier domestique s'est pris les pates dans le fil électrique de mon radio-réveil qui n'a donc pas sonné,");
+
+		var corpsExcuse2 = Object.create (citation);
+		corpsExcuse2.init("ma vieille grand-mère a joué avec le fil électrique de mon radio-réveil qui n'a donc pas sonné,");
+
+		var corpsExcuse3 = Object.create (citation);
+		corpsExcuse3.init("mon perroquet a joué avec le fil électrique de mon radio-réveil qui n'a donc pas sonné,");
+
+		var corpsExcuse4 = Object.create (citation);
+		corpsExcuse4.init("mon chien a effleuré par inadvertance le Snooze de mon radio-réveil qui n'a donc pas sonné,");
+
+		var corpsExcuse5 = Object.create (citation);
+		corpsExcuse5.init("ma belle-mère a renversé du Coca sur mon radio-réveil qui n'a donc pas sonné,");
+
+		var corpsExcuse6 = Object.create (citation);
+		corpsExcuse6.init("mon iguane asthmatique a rebooté mon radio-réveil qui n'a donc pas sonné,");
+
+
+		var corpsExcuseAll = [];
+		corpsExcuseAll.push (corpsExcuse1);
+		corpsExcuseAll.push (corpsExcuse2);
+		corpsExcuseAll.push (corpsExcuse3);
+		corpsExcuseAll.push (corpsExcuse4);
+		corpsExcuseAll.push (corpsExcuse5);
+		corpsExcuseAll.push (corpsExcuse6);
+
+		// Conslusions excuses reveil
+
+		var conclusionExcuse1 = Object.create (citation);
+		conclusionExcuse1.init("et ce n'est que quand le plombier est venu réparer l'inondation que je me suis réveillé.");
+
+		var conclusionExcuse2 = Object.create (citation);
+		conclusionExcuse2.init("et ce n'est que lorsque les pompiers sont entrés en hurlant 'AU FEU!' que j'ai bondi hors de mon lit pour me ruer au bureau.");
+
+		var conclusionExcuse3 = Object.create (citation);
+		conclusionExcuse3.init("et ce n'est qu'avec la visite d'un représentant du Téléthon venu me remercier pour mon généreux don de la veille que j'ai réalisé qu'il était trop tard pour venir au bureau ce matin.");
+
+		var conclusionExcuse4 = Object.create (citation);
+		conclusionExcuse4.init("et ce n'est qu'au moment où les huissiers (venus pour le voisin) ont enfoncé la porte que j'ai bondi hors de mon lit pour me ruer au bureau.");
+
+		var conclusionExcuse5 = Object.create (citation);
+		conclusionExcuse5.init("et ce n'est qu'avec l'arrivée du SAMU, venu chercher ma grand-mère que je me suis réveillé.");
+
+		var conclusionExcuse6 = Object.create (citation);
+		conclusionExcuse6.init("et ce n'est que quand le plombier est venu réparer l'inondation que j'ai repris connaissance.");
+
+
+		var conclusionExcuseAll = [];
+		conclusionExcuseAll.push (conclusionExcuse1);
+		conclusionExcuseAll.push (conclusionExcuse2);
+		conclusionExcuseAll.push (conclusionExcuse3);
+		conclusionExcuseAll.push (conclusionExcuse4);
+		conclusionExcuseAll.push (conclusionExcuse5);
+		conclusionExcuseAll.push (conclusionExcuse6);
+
+// FONCTIONS - GENERATIONS
+
+		function getRandomInt(arrayName) {
+		    return Math.floor(Math.random() * (arrayName.length - 1));
+		};
+
+		function generateCitation(option) {
+		    var citations = [];
+		    switch (option) {
+		        case 'citationsIntelligentes':
+		            citations = introAll[getRandomInt(introAll)].texte + " " + corpsAll[getRandomInt(corpsAll)].texte + " " + conclusionAll[getRandomInt(conclusionAll)].texte + '<br/><br/>';
+		            break;
+		        case 'citationsRetard':
+		            citations = introExcuseAll[getRandomInt(introExcuseAll)].texte + " " + corpsExcuseAll[getRandomInt(corpsExcuseAll)].texte + " " + conclusionExcuseAll[getRandomInt(conclusionExcuseAll)].texte +'<br/><br/>';
+		            break;
+		    }
+		    return citations;
+		};
+
+		function doItCitation(option, nombre) {
+		    var resultcitation = [];
+		    for (var i = 0; i < nombre; i++) {
+		        resultcitation.push(generateCitation(option));
+		    }
+		    return resultcitation;
+		};
+	
+// VERIFICATION DU FORMULAIRE
+
+	function verifTypeCitation() {
+		var intelligence = document.getElementById("citationsIntelligentes").checked;
+		var retard = document.getElementById("citationsRetard").checked;
+
+			if (intelligence == false && retard == false) {
+			  alert('Veuillez selectionner un type de citation à générer');
+			}
+	}
+
+	function verifNbCitation() {
+		var nbcitation = document.getElementById("nbCitations").value;
+		submitOK = "true";
+
+			if (nbcitation == 0) {
+			  alert('Veuillez selectionner un nombre de citation à générer');
+			  	submitOK = "false";
+			}
+			
+			if (submitOK == "false") {
+        	return false;
+    		}
+	}
+
+	function displayReponse() {
+		submitOK = "true";
+		var radiosButtons = document.getElementsByName('typeCitation');
+		var selectUserTypeCitation;
+			for(var i = 0; i < radiosButtons.length; i++){
+		 		if(radiosButtons[i].checked){
+		 		selectUserTypeCitation = radiosButtons[i].value;
+		 	}
+		};
+
+		var selectUserNbCitation = document.getElementById("nbCitations").value;
+		
+		document.getElementById('resultat_citations').innerHTML ="";
+		doItCitation(selectUserTypeCitation, selectUserNbCitation).forEach(function(element){
+			document.getElementById('resultat_citations').innerHTML += element;
+		})
+	};
+
+// RELANCE GENERATEUR CITATION
+
+	function relauchGenerator() {
+	    if (confirm("Souhaitez-vous générer de nouvelles citations ?")) {
+	        location.reload(true);
+	    }
+	};
